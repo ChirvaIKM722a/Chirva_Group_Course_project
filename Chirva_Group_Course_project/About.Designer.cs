e92055bf3ab3fe7cafebdc96e72974784d955171
Daniil <chirvadana@gmail.com>
2024-04-08 02:12:29 +0300

    Finish Task number 2

---
+++ Chirva_Group_Course_project/About.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Chirva_Group_Course_project
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tAbout = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Закрити";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(545, 78);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Розробив:\r\nСтудент групи ІКМ 722а\r\nЧирва Даніїл Віталійович\r\n\r\nТекст завдання:\r\nВ" +
+    "водиться 5 дійсних чисел. Вивести на екран число, у якого середнє арифметичне йо" +
+    "го чисел найбільше.\r\n";
+            // 
+            // tAbout
+            // 
+            this.tAbout.Interval = 10000;
+            this.tAbout.Tick += new System.EventHandler(this.tAbout_Tick);
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 457);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "About";
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Timer tAbout;
+    }
+}
